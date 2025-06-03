502be3c65f24211c6c7888a92a7fa1409333c729
Hugo Florentino <160500311+HugoAFlorentino@users.noreply.github.com>
2025-06-03 21:46:27 +0100

    Delete backend directory

--- backend/routes/logRoutes.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-import { getLogs } from '../controllers/log.controllers.js';
-import authenticateUser from '../middleware/authUser.js';
-
-const logRouter = Router();
-
-logRouter.get('/logs', authenticateUser, getLogs);
-
-export default logRouter;
--- backend/routes/subscriptionRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sendThankYou } from '../controllers/subscription.controller.js';
-
-const subscriptionRouter = Router();
-
-subscriptionRouter.post('/subscription/thank-you', sendThankYou);
-
-export default subscriptionRouter;
