71d588b0c4c5f9be75cbf7148e1da8ef368d40c4
Hugo <hugoflorentino86@hotmail.com>
2025-06-04 15:45:43 +0100

    trying to fix the redirect after forgot password

--- project-root/frontend/src/utils/axios.js
+++ project-root/frontend/src/utils/axios.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log('🔧 VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
