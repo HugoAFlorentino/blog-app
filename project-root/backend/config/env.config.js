fa70f28bdbff4c4432ae1814268a5e9b11c082cc
Hugo <hugoflorentino86@hotmail.com>
2025-06-04 14:45:36 +0100

    trying to fix bug with render connecting

--- project-root/backend/config/env.config.js
+++ project-root/backend/config/env.config.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 
-config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+}
 
 export const PORT = process.env.PORT || 8080;
 
