2df2eb8c0768e2e63e8344790141911c225612b2
Hugo <hugoflorentino86@hotmail.com>
2025-06-04 19:48:22 +0100

    trying to fix vite config problem

--- project-root/frontend/vite.config.js
+++ project-root/frontend/vite.config.js
@@ -1,23 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react-vendor';
-            if (id.includes('@reduxjs')) return 'redux-vendor';
-            if (id.includes('framer-motion')) return 'framer-motion';
-            if (id.includes('react-router-dom')) return 'router-vendor';
-            if (id.includes('react-icons')) return 'icons-vendor';
-            if (id.includes('axios')) return 'axios-vendor';
-            return 'vendor';
-          }
-        },
-      },
+  resolve: {
+    alias: {
+      react: resolve(__dirname, 'node_modules/react'),
+      'react-dom': resolve(__dirname, 'node_modules/react-dom'),
     },
   },
 });
