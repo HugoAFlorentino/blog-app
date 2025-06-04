d8a606e3871c2ac248a92f87f169ce9a1714c8f4
Hugo <hugoflorentino86@hotmail.com>
2025-06-04 19:31:58 +0100

    change vite config to compress large chuncks

--- project-root/frontend/vite.config.js
+++ project-root/frontend/vite.config.js
@@ -3,27 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // temporarily comment out the custom chunk splitting
-  /*
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'vendor_react';
-            }
-            if (id.includes('redux') || id.includes('@reduxjs')) {
-              return 'vendor_redux';
-            }
-            if (id.includes('framer-motion')) {
-              return 'vendor_framer_motion';
-            }
-            return 'vendor_other';
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('@reduxjs')) return 'redux-vendor';
+            if (id.includes('framer-motion')) return 'framer-motion';
+            if (id.includes('react-router-dom')) return 'router-vendor';
+            if (id.includes('react-icons')) return 'icons-vendor';
+            if (id.includes('axios')) return 'axios-vendor';
+            return 'vendor';
           }
         },
       },
     },
   },
-  */
 });
