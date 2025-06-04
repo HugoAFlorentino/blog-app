80c3f0b2a54bbb873e23f7166dbf36945ebe198b
Hugo <hugoflorentino86@hotmail.com>
2025-06-04 19:51:35 +0100

    fixing minor bugs

--- project-root/frontend/vite.config.js
+++ project-root/frontend/vite.config.js
@@ -14,4 +14,22 @@ export default defineConfig({
       'react-dom': resolve(__dirname, 'node_modules/react-dom'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600, // Increase chunk size warning limit to 600 KB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('@reduxjs')) return 'redux-vendor';
+            if (id.includes('framer-motion')) return 'framer-motion';
+            if (id.includes('react-router-dom')) return 'router-vendor';
+            if (id.includes('react-icons')) return 'icons-vendor';
+            if (id.includes('axios')) return 'axios-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
